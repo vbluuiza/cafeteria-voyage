215c099670cd9b0962dbb9a41be7d541880cd7e5
vbluuiza <luizavieirabelo3008@gmail.com>
2025-06-04 14:04:18 -0300

    ✨ feat: atualiza run.py para suporte de deploy na Render usando banco SQLite local

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from main import criar_app,db
 from flask import Flask
+import os 
+
+os.makedirs("instance", exist_ok=True)
 
 app = criar_app()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
