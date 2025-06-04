e0c20f4fbb4ad34b5005521d9dd3b7a7ff3787e1
vbluuiza <luizavieirabelo3008@gmail.com>
2025-06-04 13:18:51 -0300

    🐛 fix(login): corrigido erro 403 e renomeado para index.html para compatibilidade com deploy
     login.html foi renomeado para index.html para evitar problemas de carregamento inicial em plataformas como Vercel ou GitHub Pages
    Corrigido caminho da imagem Voyage-tag.jpg com uso de url_for
    Removido uso de '../static/...' que causava erro 403
    Testado com sucesso em guia anônima

--- app/views/initial_view.py
+++ app/views/initial_view.py
@@ -4,4 +4,4 @@ login_bp = Blueprint('login', __name__, url_prefix='/')
 
 @login_bp.route('/')
 def ver_pedidos_feitos():
-    return render_template('login.html')
+    return render_template('index.html')
